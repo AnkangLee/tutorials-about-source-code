f30d1c5caa26523d6840c49851d15a49fd113eab
LeeAnkang <leeankang@163.com>
2019-08-01 09:58:08 +0800

    feat(beans): xml 文件配置的方式使用spring

---
+++ spring-annotation-tutorial/src/main/java/dev/ankang/beans/Person.java
@@ -0,0 +1,23 @@
+package dev.ankang.beans;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ *
+ * description :
+ * last-modified : 2019-08-01
+ *
+ * @author LeeAnkang
+ * @version 1.0.0
+ */
+@ToString
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Person {
+    private String name;
+    private Integer age;
+}
---
+++ spring-annotation-tutorial/src/test/java/TestSpringXmlConfig.java
@@ -0,0 +1,22 @@
+import org.junit.Test;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * description :
+ * last-modified : 2019-08-01
+ *
+ * @author LeeAnkang
+ * @version 1.0.0
+ */
+public class TestSpringXmlConfig {
+
+
+    @Test
+    /**
+     * xml 文件配置的方式使用spring
+     */
+    public void testSpringXmlConfig(){
+        ClassPathXmlApplicationContext ac = new ClassPathXmlApplicationContext("beans.xml");
+        System.out.println(ac.getBean("person"));
+    }
+}
